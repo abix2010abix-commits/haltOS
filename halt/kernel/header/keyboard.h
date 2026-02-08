87437119098a44d59cf5e38578c81a9e5cf27563
abix2010abix-commits <abix.2010.abix@gmail.com>
2026-02-08 22:17:50 +0100

    Delete halt directory

--- halt/kernel/header/keyboard.h
+++
@@ -1,5 +0,0 @@
-// ========================================
-// keyboard header file 7/1/2026
-// ========================================
-unsigned char scan_click_key();
-unsigned char tran_code(unsigned code_hex);
